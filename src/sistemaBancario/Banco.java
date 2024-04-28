994f659737d4bcd0777fca8c2634a7587e4e2bcf
UCBGABRIEL <103432588+UCBGABRIEL@users.noreply.github.com>
2024-04-27 23:54:49 -0300

    update banco

--- src/sistemaBancario/Banco.java
+++ src/sistemaBancario/Banco.java
@@ -1,5 +1,13 @@
 package sistemaBancario;
 
 public class Banco {
-
+    public synchronized static void transferir(Conta contaOrigem, Conta contaDestino, double valor) {
+        if (contaOrigem.getSaldo() >= valor) {
+            contaOrigem.sacar(valor);
+            contaDestino.depositar(valor);
+            System.out.println("Transferência no valor de R$" + valor + " realizada de " + contaOrigem.getTitular() + " para a(o)" + contaDestino.getTitular());
+        } else {
+            System.out.println("Saldo insuficiente para transferência, enviou " + contaOrigem.getTitular() + " para " + contaDestino.getTitular());
+        }
+    }
 }
