0ed93d029eee1cac5d27d509de758aa6df481452
UCBGABRIEL <103432588+UCBGABRIEL@users.noreply.github.com>
2024-04-27 23:58:32 -0300

    get do Saldo e Titular

--- src/sistemaBancario/Conta.java
+++ src/sistemaBancario/Conta.java
@@ -1,5 +1,27 @@
 package sistemaBancario;
 
 public class Conta {
+    private String titular;
+    private double saldo;
 
+    public Conta(String titular) {
+        this.titular = titular;
+        this.saldo = 0;
+    }
+
+    public synchronized void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public synchronized void sacar(double valor) {
+        saldo -= valor;
+    }
+
+    public synchronized double getSaldo() {
+        return saldo;
+    }
+
+    public synchronized String getTitular() {
+        return titular;
+    }
 }
