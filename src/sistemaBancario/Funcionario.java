3987f5f2de7122989399f73785de9a836e258ac9
UCBGABRIEL <103432588+UCBGABRIEL@users.noreply.github.com>
2024-04-28 00:00:55 -0300

    Classe funcionario completa

--- src/sistemaBancario/Funcionario.java
+++ src/sistemaBancario/Funcionario.java
@@ -1,5 +1,28 @@
 package sistemaBancario;
 
-public class Funcionario {
+package sistemaBancario;
 
+public class Funcionario extends Thread {
+    private static final double SALARIO = 1400;
+    private static final double TAXA_INVESTIMENTO = 0.2;
+    private Conta contaSalario;
+    private Conta contaInvestimento;
+
+    public Funcionario(Conta contaSalario, Conta contaInvestimento) {
+        this.contaSalario = contaSalario;
+        this.contaInvestimento = contaInvestimento;
+    }
+
+    @Override
+    public void run() {
+        synchronized (contaSalario) {
+            contaSalario.depositar(SALARIO);
+            System.out.println(Thread.currentThread().getName() + " recebeu o salário de R$" + SALARIO);
+        }
+        synchronized (contaInvestimento) {
+            double valorInvestimento = SALARIO * TAXA_INVESTIMENTO;
+            contaInvestimento.depositar(valorInvestimento);
+            System.out.println(Thread.currentThread().getName() + " investiu R$" + valorInvestimento + " em sua conta de investimento");
+        }
+    }
 }
