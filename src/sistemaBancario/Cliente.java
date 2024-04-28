ee47ae34fbbba5c1cbafdf67c5e1af7b6e506739
UCBGABRIEL <103432588+UCBGABRIEL@users.noreply.github.com>
2024-04-27 23:57:28 -0300

    Cliente threads

--- src/sistemaBancario/Cliente.java
+++ src/sistemaBancario/Cliente.java
@@ -1,5 +1,33 @@
 package sistemaBancario;
 
-public class Cliente {
+import java.util.Random;
 
+public class Cliente extends Thread {
+	private static final int SALARIO_MINIMO = 1400;
+    private Conta conta;
+
+    public Cliente(Conta conta) {
+        this.conta = conta;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        while (true) {
+            int valorCompra = random.nextInt(2) == 0 ? 100 : 200;
+            synchronized (conta) {
+                if (conta.getSaldo() >= valorCompra) {
+                    conta.sacar(valorCompra);
+                    System.out.println(Thread.currentThread().getName() + " realizou uma compra de R$" + valorCompra);
+                } else {
+                    break;
+                }
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
