e7b0ab8be6e761b8e5c77ecdc24ba77d18a8b0b2
UCBGABRIEL <103432588+UCBGABRIEL@users.noreply.github.com>
2024-04-28 00:01:35 -0300

    Classe loja com os get e tudo certo

--- src/sistemaBancario/Loja.java
+++ src/sistemaBancario/Loja.java
@@ -1,5 +1,21 @@
 package sistemaBancario;
 
 public class Loja {
+    private Conta conta;
 
+    public Loja(Conta conta) {
+        this.conta = conta;
+    }
+
+    public void pagarSalarios() {
+        synchronized (conta) {
+            double totalSalarios = Funcionario.SALARIO * 2; // Duas lojas, dois funcionários cada
+            if (conta.getSaldo() >= totalSalarios) {
+                conta.sacar(totalSalarios);
+                System.out.println("Salários dos funcionários pagos com sucesso.");
+            } else {
+                System.out.println("Não há saldo suficiente para pagar os salários dos funcionários.");
+            }
+        }
+    }
 }
